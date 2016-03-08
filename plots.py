59444ff229baf31891b1dfb416417dd1c7391988
Jonny H Williams <williamsjh@climate-1.(none)>
2016-03-08 00:57:41 +0000

    Changing file name.

--- plots.py
+++ plots.py
@@ -7,7 +7,7 @@ import iris.quickplot as qplt
 
 fname = '/home/williamsjh/cylc-run/niwa_update/share/data/History_Data/niwa_a.pd1981sep'
 
-cube_all = iris.load(fname, 'air_temperature')
+cube_all = iris.load(fname, 'specific_humidity')
 
 cube=cube_all[0]
 print(cube)
@@ -20,10 +20,6 @@ with iris.FUTURE.context(cell_datetime_objects=True):
     cube_1 = cube.extract(day_1)
 
 print(cube_1)
-
-
-
-
 
 fig = plt.figure()
 qplt.contour(cube_1)
