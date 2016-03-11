829c32a60ecdaf705f431f7a791c29658e7a0214
Jonny H Williams <williamsjh@climate-1.(none)>
2016-03-11 01:49:08 +0000

    Further work on indexing.

--- plots.py
+++ plots.py
@@ -4,6 +4,8 @@ import iris
 import iris.plot as iplt
 import iris.quickplot as qplt
 
+fig = plt.figure()
+
 for i in range(0,2):
 
     fname = '/home/williamsjh/cylc-run/u-ab642/share/data/History_Data/ab642a.pd1981sep'
@@ -11,25 +13,22 @@ for i in range(0,2):
     cube_all = iris.load(fname, 'specific_humidity')
 
     cube=cube_all[0]
-    print(cube)
-    print('All times :\n') 
-    print(cube.coord('time'))
+#    print(cube)
+#    print('All times :\n') 
+#    print(cube.coord('time'))
 
     day_1 = iris.Constraint(time=lambda cell: cell.point.day == 1)
 
     with iris.FUTURE.context(cell_datetime_objects=True):
         cube_1 = cube.extract(day_1)
 
-    print(cube_1)
+#    print(cube_1)
+    
+    print(i+1)
 
-j=1
+    plt.subplot(1,2,i+1)
+    qplt.pcolor(cube_1, cmap='RdBu')
+    plt.gca().coastlines()
 
-fig = plt.figure()
-plt.subplot(1,2,j)
-qplt.pcolor(cube_1, cmap='RdBu')
-plt.gca().coastlines()
-# plt.subplot(122)
-# qplt.pcolormesh(cube_1)
-# plt.gca().coastlines()
 iplt.show()
 
