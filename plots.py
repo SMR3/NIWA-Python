77242a3ce1387606230be20a04df1def00ce17d1
Jonny H Williams <williamsjh@climate-1.(none)>
2016-03-11 01:06:59 +0000

    Introducing looping.

--- plots.py
+++ plots.py
@@ -1,28 +1,29 @@
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
-
 import iris
 import iris.plot as iplt
 import iris.quickplot as qplt
 
-fname = '/home/williamsjh/cylc-run/u-ab642/share/data/History_Data/ab642a.pd1981sep'
+for i in range(0,2):
 
-cube_all = iris.load(fname, 'specific_humidity')
+    fname = '/home/williamsjh/cylc-run/u-ab642/share/data/History_Data/ab642a.pd1981sep'
 
-cube=cube_all[0]
-print(cube)
-print('All times :\n') 
-print(cube.coord('time'))
+    cube_all = iris.load(fname, 'specific_humidity')
 
-day_1 = iris.Constraint(time=lambda cell: cell.point.day == 1)
+    cube=cube_all[0]
+    print(cube)
+    print('All times :\n') 
+    print(cube.coord('time'))
 
-with iris.FUTURE.context(cell_datetime_objects=True):
-    cube_1 = cube.extract(day_1)
+    day_1 = iris.Constraint(time=lambda cell: cell.point.day == 1)
 
-print(cube_1)
+    with iris.FUTURE.context(cell_datetime_objects=True):
+        cube_1 = cube.extract(day_1)
+
+    print(cube_1)
 
 fig = plt.figure()
-# plt.subplot(121)
+plt.subplot(121)
 qplt.pcolor(cube_1, cmap='RdBu')
 plt.gca().coastlines()
 # plt.subplot(122)
