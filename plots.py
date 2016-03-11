3c4a2388f344ee6951b57c11845fc3b0018e2867
Jonny H Williams <williamsjh@climate-1.(none)>
2016-03-11 00:58:34 +0000

    Changing to gridded output and other misc. changes.

--- plots.py
+++ plots.py
@@ -5,7 +5,7 @@ import iris
 import iris.plot as iplt
 import iris.quickplot as qplt
 
-fname = '/home/williamsjh/cylc-run/niwa_update/share/data/History_Data/niwa_a.pd1981sep'
+fname = '/home/williamsjh/cylc-run/u-ab642/share/data/History_Data/ab642a.pd1981sep'
 
 cube_all = iris.load(fname, 'specific_humidity')
 
@@ -22,7 +22,11 @@ with iris.FUTURE.context(cell_datetime_objects=True):
 print(cube_1)
 
 fig = plt.figure()
-qplt.contour(cube_1)
+# plt.subplot(121)
+qplt.pcolor(cube_1, cmap='RdBu')
 plt.gca().coastlines()
+# plt.subplot(122)
+# qplt.pcolormesh(cube_1)
+# plt.gca().coastlines()
 iplt.show()
 
