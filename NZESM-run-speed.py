6efd0599de15afbdd791cbddd4a0fc9becad676a
Jonny H Williams <williamsjh@climate-1.(none)>
2016-04-01 00:55:29 +0000

    Plotting new dummy arrays

--- NZESM-run-speed.py
+++ NZESM-run-speed.py
@@ -8,19 +8,19 @@ processor_decomposition=[16*8,16*16,16*32]
 
 n96=np.array([(36.*60.+59.)-(29.*60.+43.),(21.*60.+24.)-(17.*60.+1.),(28.*60.+37.)-(25.*60.+20.)])
 
-n96-chem=np.array([(.*60.+.)-(.*60.+.),(.*60.+.)-(.*60.+.),(.*60.+.)-(.*60.+.)])
+n96_chem=n96*1.1#np.array([(.*60.+.)-(.*60.+.),(.*60.+.)-(.*60.+.),(.*60.+.)-(.*60.+.)])
 
-n216=np.array([(.*60.+.)-(.*60.+.),(.*60.+.)-(.*60.+.),(.*60.+.)-(.*60.+.)])
-
-print(n96)
+n216=n96*1.2#np.array([(.*60.+.)-(.*60.+.),(.*60.+.)-(.*60.+.),(.*60.+.)-(.*60.+.)])
 
 #'n96' is the number of seconds taken to run one day of simulation. We need to convert this to years of simulation per wallclock day.
 
-ypd=1./n96/360.*(24.*60.*60.)
+ypd_n96=1./n96/360.*(24.*60.*60.)
+ypd_n96_chem=1./n96_chem/360.*(24.*60.*60.)
+ypd_n216=1./n216/360.*(24.*60.*60.)
 
-print(ypd)
-
-plt.plot(processor_decomposition,ypd,'-o')
+plt.plot(processor_decomposition,ypd_n96,'-ok',ms=10)
+plt.plot(processor_decomposition,ypd_n96_chem,'-sk',ms=10)
+plt.plot(processor_decomposition,ypd_n216,'-^k',ms=10)
 plt.xlabel('number of processors')
 plt.ylabel('years $\cdot$ day $^{-1}$')
 plt.grid(True)
