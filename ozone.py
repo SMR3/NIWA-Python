d0a5f5973cdc47bf6b94c53ee3152f9eef190b4f
williamsjh <williamsjh@climate-1.(none)>
2016-05-02 23:24:44 +0000

    Adding in 2nd plot

--- ozone.py
+++ ozone.py
@@ -12,21 +12,21 @@ import numpy as np
 
 fig = plt.figure()
 
-dirnames=np.array(['u-ab642'])
+dirnames=np.array(['u-ab642','niwa_update_NIWA-UKCA_refC1_2001-2010_zm'])
 fnames=np.array(['ab642'])
 
 varnames=np.array([\
 'air_temperature',\
 ])
 
-for i in range(0,len(varnames)):
+for i in range(0,len(dirnames)):
 
-    fname = '/home/williamsjh/cylc-run/'+dirnames[0]+'/share/data/History_Data/'+fnames[0]+'a.ps1981son'
+    fname = '/home/williamsjh/cylc-run/'+dirnames[i]+'/share/data/History_Data/'+fnames[0]+'a.ps1981son'
 
-    cube_all = iris.load(fname, varnames[i])
+    cube_all = iris.load(fname, varnames[0])
     cube=cube_all[0] 
     
-    plt.subplot(1,len(varnames),i+1)
+    plt.subplot(1,2,i+1)
     qplt.pcolormesh(cube, cmap='RdBu_r')
     plt.title('' )
     plt.gca().coastlines()
