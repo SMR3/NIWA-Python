cfe45353f8bdc31b127141d899ced11c7ceb9cfc
williamsjh <williamsjh@climate-1.(none)>
2016-05-19 04:21:17 +0000

    Adding plot to have two 'controls'; one NIWA one UKMO

--- ozone.py
+++ ozone.py
@@ -1,6 +1,6 @@
 fig = plt.figure(figsize=(10,10))
 
-dirnames = np.array(['u-ab642','niwa_update_NIWA-UKCA_refC1_2001-2010_zm','niwa_update_NIWA-UKCA_refC1_2001-2010','niwa_update_NIWA-ozone-forcing-from-anqdg-2001-2010_zm','niwa_update_NIWA-ozone-forcing-from-anqdg-2001-2010'])
+dirnames = np.array(['dummy','u-ab642-10-years','niwa_update_NIWA-UKCA_refC1_2001-2010_zm','niwa_update_NIWA-UKCA_refC1_2001-2010','niwa_update_NIWA-ozone-forcing-from-anqdg-2001-2010_zm','niwa_update_NIWA-ozone-forcing-from-anqdg-2001-2010'])
 fnames = np.array(['ab642'])
 titles=['control','UKCA refC1 zm - control','UKCA refC1 - control','anqdg zm - control','anqdg - control']
 
@@ -10,7 +10,10 @@ varnames = np.array([\
 
 for i in range(0,len(dirnames)):
 
-    fname = '/home/williamsjh/cylc-run/'+dirnames[i]+'/share/data/History_Data/'+fnames[0]+'a.ps1983jja'
+    if i == 0:
+        fname = '/hpcf/data/williamsjh/MASS/ab642a.px19911201.pp'
+    else: 
+        fname = '/home/williamsjh/cylc-run/'+dirnames[i]+'/share/data/History_Data/'+fnames[0]+'a.py19901201'
 
     cube_all = iris.load(fname, varnames[0])
     cube=cube_all[0] 
@@ -25,10 +28,10 @@ for i in range(0,len(dirnames)):
         lonplot,latplot=np.meshgrid(lon,lat)
     else:
         plotthis = cube - cube_control
-        vmin = -5
-        vmax = 5
+        vmin = -1
+        vmax = 1
 
-    plt.subplot(3,2,i + 1 + (i > 0))
+    plt.subplot(3,2,i + 1)
 
 # 1 - Plot with Iris qplot
     qplt.pcolormesh(plotthis, cmap='RdBu_r', vmin=vmin, vmax=vmax)
